212cf5c55b76c360b876624d3b435ae75f35b829
Nikita <Nikita.kuzin.88@mail.ru>
2023-07-22 16:28:05 +0500

    add IntroArray

---
+++ Example003_IntroArray/Program.cs
@@ -0,0 +1,19 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int []  array =  {4522,34,23,12,54,345,23,12,23};
+//array[0] = 12;// как обратитс к массиву
+//Console.WriteLine(array[0]);//так выводить значение массива на консоль
+
+int result = Max (
+    Max (array [0], array [1], array [2]),
+    Max (array [3], array [4], array [5]),
+    Max (array [6], array [7], array [8]));
+
+    Console.WriteLine(result);
+
